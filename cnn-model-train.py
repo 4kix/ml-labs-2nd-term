448e54d7ab1b402efc52ffae110b97861947f556
4kix <falko.ilya@gmail.com>
2020-04-09 20:08:08 +0300

    Lab 4. Try to learn on SVHN

--- cnn-model-train.py
+++ cnn-model-train.py
@@ -2,7 +2,9 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.utils import np_utils
+from scipy.io import loadmat
 
+import numpy as np
 import tensorflow as tf
 import keras.backend.tensorflow_backend as tfback
 
@@ -32,23 +34,49 @@ drop_prob_1 = 0.25
 drop_prob_2 = 0.5
 hidden_size = 512
 
-num_train = 60000
-num_test = 10000
+# num_train = 60000
+# num_test = 10000
 
-height, width, depth = 28, 28, 1
+height, width, depth = 32, 32, 1
 num_classes = 10
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+def load_data(path):
+    data = loadmat(path)
+    return data['X'], data['y']
 
-X_train = X_train.reshape(num_train, height, width, depth)
-X_test = X_test.reshape(num_test, height, width, depth)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255 # Normalise data to [0, 1] range
-X_test /= 255 # Normalise data to [0, 1] range
+def rgb2gray(images):
+    return np.expand_dims(np.dot(images, [0.2990, 0.5870, 0.1140]), axis=3)
 
-Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
-Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+X_train, y_train = load_data("train_32x32.mat")
+X_test, y_test = load_data("test_32x32.mat")
+
+# num_classes = np.unique(y_train).shape[0]
+
+
+# Reshape the image arrays
+X_train, y_train = np.rollaxis(X_train, 3), y_train[:,0]
+X_test, y_test = np.rollaxis(X_test, 3), y_test[:,0]
+
+# print("test shape",X_test.shape)
+
+X_train_gs = rgb2gray(X_train)
+X_test_gs = rgb2gray(X_test)
+
+# num_train = X_train.shape[0]
+# num_test = X_test.shape[0]
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# X_train = X_train.reshape(num_train, height, width, depth)
+# X_test = X_test.reshape(num_test, height, width, depth)
+X_train_gs = X_train_gs.astype('float32')
+X_test_gs = X_test_gs.astype('float32')
+X_train_gs /= 255 # Normalise data to [0, 1] range
+X_test_gs /= 255 # Normalise data to [0, 1] range
+
+y_train[y_train==10] = 0
+y_test[y_test==10] = 0
+# Y_train = np_utils.to_categorical(y_train) # One-hot encode the labels
+# Y_test = np_utils.to_categorical(y_test) # One-hot encode the labels
 
 model = Sequential()
 model.add(Convolution2D(conv_depth_1, kernel_size, kernel_size, border_mode='same', activation='relu'))
@@ -64,11 +92,11 @@ model.add(Dense(hidden_size, activation='relu'))
 model.add(Dropout(drop_prob_2))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+model.compile(loss='sparse_categorical_crossentropy', # using the cross-entropy loss function
               optimizer='adam', # using the Adam optimiser
               metrics=['accuracy']) # reporting the accuracy
 
-model.fit(X_train, Y_train,
+model.fit(X_train_gs, y_train,
           batch_size=batch_size, nb_epoch=num_epochs,
           verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
-model.evaluate(X_test, Y_test, verbose=1) # Evaluate the trained model on the test set!
+model.evaluate(X_test_gs, y_test, verbose=1) # Evaluate the trained model on the test set!
